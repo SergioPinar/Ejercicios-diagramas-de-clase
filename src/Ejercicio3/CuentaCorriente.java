5038ce7b1c616e93a654e831e5a9300e18bd54ce
Sergio <sergiopi137@gmail.com>
2025-02-27 22:07:43 +0100

    Guardando lo que no se ha guardado

--- src/Ejercicio3/CuentaCorriente.java
+++ src/Ejercicio3/CuentaCorriente.java
@@ -1,5 +1,23 @@
 package Ejercicio3;
 
-public class CuentaCorriente {
+public class CuentaCorriente extends CuentaBancaria {
+    private double limiteDescubierto;
 
+    public CuentaCorriente(String numero, String titular, double saldo, double limiteDescubierto) {
+        super(numero, titular, saldo);
+        this.limiteDescubierto = limiteDescubierto;
+    }
+
+    @Override
+    public void retirar(double cantidad) {
+        if (cantidad > 0 && (saldo - cantidad) >= -limiteDescubierto) {
+            saldo -= cantidad;
+        }
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        super.mostrarInformacion();
+        System.out.println("Límite de descubierto: " + limiteDescubierto);
+    }
 }
